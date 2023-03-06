9c68d06574eb579235b0c7dc23decd57bb5d8489
Elmo <elmotabuzojr.ust@gmail.com>
2023-03-06 15:38:19 +0800

    linking between pages using Link

--- nextjs-course/src/pages/news/index.js
+++ nextjs-course/src/pages/news/index.js
@@ -1,9 +1,19 @@
 // our-domain.com/news
 
+import Link from 'next/link';
 import React from 'react';
 
 const NewsPage = () => {
-  return <h1>NewsPage</h1>;
+  return (
+    <>
+      <ul>
+        <li>
+          <Link href='/news/1'>NextJS is a great framework</Link>
+        </li>
+        <li>Something</li>
+      </ul>
+    </>
+  );
 };
 
 export default NewsPage;
