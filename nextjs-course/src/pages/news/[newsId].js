e987b641fcccc06322dd4bade170d3e0476b51b1
Elmo <elmotabuzojr.ust@gmail.com>
2023-03-06 15:32:54 +0800

    getting dynamic parameter values

--- nextjs-course/src/pages/news/[newsId].js
+++ nextjs-course/src/pages/news/[newsId].js
@@ -1,8 +1,14 @@
-// our-domain.com/news
+// our-domain.com/news/??
 
+import { useRouter } from 'next/router';
 import React from 'react';
 
 const DetailsPage = () => {
+  const router = useRouter();
+
+  // extracting parameter values newsId because thats the file name
+  console.log(router.query.newsId);
+
   return <h1>DetailsPage</h1>;
 };
 
